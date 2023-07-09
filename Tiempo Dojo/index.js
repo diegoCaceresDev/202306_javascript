3085d7a7fdfe992ff2f9b53c5f740abe3106e596
Druyeah <you@example.com>
2023-07-09 14:29:13 -0400

    subo tarea de tiempo dojo completa

--- Tiempo Dojo/index.js
+++ Tiempo Dojo/index.js
@@ -1,5 +1,28 @@
-function setName(element) {
-  var nameTag = document.querySelector("#name-tag");
+$(".button").click(function() {
+  $(".cookie").hide()
+})
 
-    nameTag.innerText = element.value;
-}
+$("#temperature").change(function() {
+  let option = $("#temperature  option:selected").text();
+
+  if (option == "°F") {
+    
+    $('.number').each(function() {
+      let celsius = parseFloat($(this).text());
+      let fahrenheit = (celsius * 9/5) + 32;
+      $(this).text(fahrenheit.toFixed(0) + 'F')})
+
+  } else if(option == "°C") {
+
+    $('.number').each(function() {
+      var fahrenheit = parseFloat($(this).text());
+      var celsius = (fahrenheit - 32) * 5/9;
+      $(this).text(celsius.toFixed(0) + 'C');
+    });
+
+  }
+})
+
+$("li").click(function(){
+  alert("Loading " + $(this).text() + " weather report...")
+})
